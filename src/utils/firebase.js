a109c499d7ba99627c87bfd77c7fd390fbae372f
assadakramiintellect <assad@iintellect.co.uk>
2019-12-26 17:34:59 +0500

    more fixes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 
 let main = firebase.initializeApp({
-    apiKey: "AIzaSyCmwvE24ptDmbwTrhMu8qsZ6NEQG5HB2Dw",
-    authDomain: "fur-baby-tracker-dev.firebaseapp.com",
-    databaseURL: "https://fur-baby-tracker-dev.firebaseio.com",
-    projectId: "fur-baby-tracker-dev",
-    storageBucket: "fur-baby-tracker-dev.appspot.com",
-    messagingSenderId: "887552431013",
-    appId: "1:887552431013:web:ab631ea8178ec5a12dce4c"
+    // apiKey: "AIzaSyCmwvE24ptDmbwTrhMu8qsZ6NEQG5HB2Dw",
+    // authDomain: "fur-baby-tracker-dev.firebaseapp.com",
+    // databaseURL: "https://fur-baby-tracker-dev.firebaseio.com",
+    // projectId: "fur-baby-tracker-dev",
+    // storageBucket: "fur-baby-tracker-dev.appspot.com",
+    // messagingSenderId: "887552431013",
+    // appId: "1:887552431013:web:ab631ea8178ec5a12dce4c"
 });
 
 export default main;
