4eebbfe99bdfa11065b90027064340c6aa3474a4
assadakramiintellect <assad@iintellect.co.uk>
2019-12-27 15:18:11 +0500

    url changed

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const apiPath = "http://127.0.0.1:8000/api/v1";
-export const apiPath = "https://us-central1.cloudfunctions.net";
+export const apiPath = "https://vft2h7l6zrbudbynw462k2ndnu.appsync-api.us-east-1.amazonaws.com/graphql";
